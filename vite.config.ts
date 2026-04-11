1ec295d36d22507870e1cc4b545a2ad1ec785a9b
Nirmal Kumar <rkp16feb2006@gmail.com>
2026-04-11 18:50:27 +0530

    final commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const plugins = [react(), tailwindcss()];
 const PROJECT_ROOT = import.meta.dirname;
 
 export default defineConfig({
+  base: "./" + (process.env.NODE_ENV === "production" ? "public/" : ""),
   plugins,
   resolve: {
     alias: {
